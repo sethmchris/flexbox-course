5bd33a24c9916d89c71b95e48b4a8bb5abc8b3f4
sethmchris <sethmchris@gmail.com>
2018-06-13 11:17:28 -0600

    created new folder for error page project, added new index.html file that includes flexbox demos

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 gulp.task('serve', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("*.html").on("change", reload);
+  browserSync.init({
+    server: {
+        baseDir: "./"
+    }
+  });
+  gulp.watch("*.html").on("change", reload);
 });
